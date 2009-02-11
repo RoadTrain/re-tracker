fe6355bc4a1ce3e0aec2c176e170eb1dea4f627b
freemandigger <freemandigger@1769e1da-f787-11dd-8259-6defae454439>
2009-02-11 09:37:59 +0000

    Changes for safe updates from svn

--- trunk/functions.php
+++ trunk/functions.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 function humn_size ($size, $rounder = null, $min = null, $space = '&nbsp;')
 {
 	static $sizes   = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
@@ -141,7 +141,7 @@ function is_url($url)
     $empty['pass'] = "";
     $empty['path'] = "";
     $empty['query'] = "";
-    
+
     if ( !( $parts = @parse_url( $url ) ) ) return false;
     else {
     	$parts = array_merge($empty, $parts);
@@ -158,63 +158,63 @@ function is_url($url)
 function city_select($city_ary, $selected_id = 0)
 {
 	$out = '';
-	
+
 	for ($i=1; $i <= ($city_ary['Количество']); $i++)
 	{
 		$out .= "<option value=\"$i\"". (($i == $selected_id) ? " selected=\"selected\"" : '') .">".
 		$city_ary[$i].
 		"</option>\n";
 	}
-	
+
 	return $out;
 }
 
 function isp_select($isp_ary, $selected_id = 0)
 {
 	$out = '<option value="0">&raquo; Выберите провайдера</option>';
-	
+
 	for ($i=1; $i <= ($isp_ary['Количество']); $i++)
 	{
 		$out .= "<option value=\"$i\"". (($i == $selected_id) ? " selected=\"selected\"" : '') .">".
 		$isp_ary[$i] ."</option>\n";
 	}
-	
+
 	return $out;
 }
 
 function tr_list($tr_ary)
 {
 	$out = '';
-	
+
 	for ($i=1; $i <= ($tr_ary['Количество']); $i++)
 	{
 		if(!strpos($tr_ary[$i], 're-tracker.ru')) $out .= $tr_ary[$i] ."\n";
 	}
-	
+
 	return $out;
 }
 
 function get_trackers()
 {
 	global $cache, $cfg;
-	
+
 	$trackers = $cache->get('trackers');
 	if (!empty($trackers))
 	{
 		return $trackers;
 	}
-	
-	$file = @file_get_contents(TRACKERS_URL);
+
+	$file = @file_get_contents($cfg['TRACKERS_URL']);
 	$file = iconv("UTF-16", "CP1251", $file);
-	
+
 	if($file)
 	{
 		$filepath = $cfg['cache']['filecache']['path']."trackers.list";
-		
+
 		file_put_contents($filepath, $file);
 		$trackers = parse_ini_file($filepath, true);
 		@unlink($filepath);
-		
+
 		$trackers_cached = $cache->set('trackers', $trackers, TRACKERS_CACHE_EXPIRE);
 	}
 	return $trackers;
@@ -371,25 +371,25 @@ function base32_encode ($inString)
                           '11110' => 0x36,
                           '11111' => 0x37,
                           );
-    
+
     /* Turn the compressed string into a string that represents the bits as 0 and 1. */
     for ($i = 0; $i < strlen($inString); $i++) {
         $compBits .= str_pad(decbin(ord(substr($inString,$i,1))), 8, '0', STR_PAD_LEFT);
     }
-    
+
     /* Pad the value with enough 0's to make it a multiple of 5 */
     if((strlen($compBits) % 5) != 0) {
         $compBits = str_pad($compBits, strlen($compBits)+(5-(strlen($compBits)%5)), '0', STR_PAD_RIGHT);
     }
-    
+
     /* Create an array by chunking it every 5 chars */
     $fiveBitsArray = split("\n",rtrim(chunk_split($compBits, 5, "\n")));
-    
+
     /* Look-up each chunk and add it to $outstring */
     foreach($fiveBitsArray as $fiveBitsString) {
         $outString .= chr($BASE32_TABLE[$fiveBitsString]);
     }
-    
+
     return $outString;
 }
 
