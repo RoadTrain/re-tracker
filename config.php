fc11ec55b98e98334d4da555043c98b0064d23c6
roadtrain4eg <roadtrain4eg@1769e1da-f787-11dd-8259-6defae454439>
2009-02-13 22:57:02 +0000

    Code formatting

--- config.php
+++ config.php
@@ -8,21 +8,21 @@ $cfg['dbname'] = "re-tracker";
 
 $tracker = 'tracker';
 $tracker_stats = 'tracker_stats';
-$search_intrv = 7; // interval for search attempts
+$search_intrv  = 7; // interval for search attempts
 $search_opt_keep = 15 * 86400; // seconds to keep search options in cookie
 $min_check_intrv = 14400; // min inverval to check torrent name
 
 
 // Tracker
-$cfg['announce_interval'] = 3600;
-$cfg['expire_factor'] = 2;
-$cfg['peers_limit'] = 100; // Limit peers to select from DB
-$cfg['cleanup_interval'] = 2400; // Interval to execute cleanup
-$cfg['compact_always'] = false; // Enable compact mode always (don't check clien capability)
+$cfg['announce_interval']  = 3600;
+$cfg['expire_factor']      = 2;
+$cfg['peers_limit']        = 100; // Limit peers to select from DB
+$cfg['cleanup_interval']   = 2400; // Interval to execute cleanup
+$cfg['compact_always']     = false; // Enable compact mode always (don't check clien capability)
 $cfg['ignore_reported_ip'] = false; // Ignore IP from GET query
-$cfg['allow_internal_ip'] = true; // Allow IP from local, etc
+$cfg['allow_internal_ip']  = true; // Allow IP from local, etc
 $cfg['verify_reported_ip'] = false; // Verify reported IP?
-$cfg['base_path'] = isset($_SERVER['DOCUMENT_ROOT'])?$_SERVER['DOCUMENT_ROOT']:dirname(realpath(__FILE__)); // Without end slash
+$cfg['base_path'] = isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : dirname(realpath(__FILE__)); // Without end slash
 $cfg['TRACKERS_URL'] = 'http://re-tracker.ru/trackerssimple.ini'; // Path to obtain tracker list
 
 
@@ -30,17 +30,27 @@ $cfg['TRACKERS_URL'] = 'http://re-tracker.ru/trackerssimple.ini'; // Path to obt
 $cfg['cache_type'] = 'filecache'; // Available cache types: none, APC, memcached, sqlite, filecache
 
 
-$cfg['cache']['memcached'] = array('host'=>'127.0.0.1', 'port'=>11211, 'pconnect'=>true, // use persistent connection
-'con_required'=>true); // exit script if can't connect
+$cfg['cache']['memcached'] = array(
+	'host'         => '127.0.0.1', 
+	'port'         => 11211, 
+	'pconnect'     => true, // use persistent connection
+	'con_required' => true
+); // exit script if can't connect
 
 
-$cfg['cache']['sqlite'] = array('db_file_path'=>'/path/to/sqlite.cache.db', #  /dev/shm/sqlite.db
-'table_name'=>'cache', 'table_schema'=>'CREATE TABLE cache (
+$cfg['cache']['sqlite'] = array(
+	'db_file_path' => '/path/to/sqlite.cache.db', #  /dev/shm/sqlite.db
+	'table_name'   => 'cache', 
+	'table_schema' => 'CREATE TABLE cache (
 	                     cache_name        VARCHAR(255),
 	                     cache_expire_time INT,
 	                     cache_value       TEXT,
 	                     PRIMARY KEY (cache_name)
-	                   )', 'pconnect'=>true, 'con_required'=>true, 'log_name'=>'CACHE');
+	                   )', 
+	'pconnect'     => true, 
+	'con_required' => true, 
+	'log_name'     => 'CACHE'
+);
 
 $cfg['cache']['filecache']['path'] = './cache_tr/';
 
@@ -62,10 +72,8 @@ define('PEERS_DATA_EXPIRE', 1500); // sec
 define('TRACKERS_CACHE_EXPIRE', 3600); // sec
 define('TORRENTS_EXPIRE', 30 * 86400); // 30 days
 
-
 // Misc
 define('DBG_LOG', false); // Debug log
-
 
 // Torrents list
 define('STATS_EXPIRE', 1200);  // sec
