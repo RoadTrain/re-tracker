505a70acced548851cc667bc334ce1efb1382c85
roadtrain4eg <roadtrain4eg@1769e1da-f787-11dd-8259-6defae454439>
2009-03-07 11:09:23 +0000

    optimize table structure

--- table.sql
+++ table.sql
@@ -4,20 +4,16 @@ CREATE TABLE IF NOT EXISTS `tracker` (
   `peer_hash` char(32) collate utf8_bin NOT NULL,
   `ip` char(15) collate utf8_bin NOT NULL,
   `ipv6` char(39) collate utf8_bin NOT NULL,
-  `port` int(11) NOT NULL,
+  `port` smallint(5) unsigned NOT NULL default '0',
   `seeder` tinyint(1) NOT NULL,
   `update_time` int(11) NOT NULL,
-  `name` varchar(255) collate utf8_bin default NULL,
-  `size` bigint(20) NOT NULL,
-  `comment` varchar(255) collate utf8_bin default NULL,
-  `pleft` bigint(16) default NULL,
+  `remain` bigint(16) default NULL,
   `downloaded` bigint(16) NOT NULL,
   `uploaded` bigint(16) NOT NULL default '0',
   `city` mediumint(2) NOT NULL,
   `isp` mediumint(2) NOT NULL,
   PRIMARY KEY  (`peer_hash`),
   KEY `torrent_id` (`torrent_id`),
-  FULLTEXT KEY `name` (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 DROP TABLE IF EXISTS `tracker_stats`;
@@ -31,7 +27,6 @@ CREATE TABLE IF NOT EXISTS `tracker_stats` (
   `name` varchar(255) NOT NULL,
   `size` bigint(20) NOT NULL,
   `comment` varchar(255) NOT NULL,
-  `check_name` tinyint(1) NOT NULL default '0',
   `last_check` int(11) NOT NULL default '0',
   PRIMARY KEY  (`torrent_id`),
   UNIQUE KEY `info_hash` (`info_hash`),
